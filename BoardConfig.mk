cc0c9204b3101f915667d0d767213cf372c491f1
Michael Bestas <mikeioannina@gmail.com>
2013-04-16 03:37:07 +0300

    Initial commit for phoenix
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,18 @@
+-include device/semc/zeus-common/BoardConfigCommon.mk
+-include vendor/semc/phoenix/BoardConfigVendor.mk
+
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/semc/msm7x30
+TARGET_KERNEL_CONFIG := cm_phoenix_defconfig
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
+
+# Recovery
+TARGET_RECOVERY_INITRC := device/semc/phoenix/recovery/init.rc
+
+# Sensors
+SOMC_CFG_SENSORS := true
+SOMC_CFG_SENSORS_COMPASS_AK8975 := yes
+SOMC_CFG_SENSORS_ACCEL_BMA150_INPUT := yes
+SOMC_CFG_SENSORS_PROXIMITY_SHARP_GP2 := yes
+
+TARGET_OTA_ASSERT_DEVICE := MT25i,MT25a,phoenix
