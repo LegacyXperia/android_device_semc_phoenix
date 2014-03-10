27cfc1351bfae993400a3ec69e54dc06b8d4e834
Michael Bestas <mikeioannina@gmail.com>
2014-03-11 01:45:17 +0200

    phoenix: Fix device name
    Change-Id: I50f11a5c0bed666114725d569a157cead51cb89a

--- cm.mk
+++ cm.mk
@@ -22,8 +22,14 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=MT25i BUILD_FINGERPRINT="SEMC/MT25i_1254-2184/MT25i:4.0.4/4.1.B.0.587/tL1_3w:user/release-keys" PRIVATE_BUILD_DESC="MT25i-user 4.0.4 4.1.B.0.587 tL1_3w test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=MT25i \
+    PRODUCT_DEVICE=MT25i \
+    BUILD_FINGERPRINT="SEMC/MT25i_1254-2184/MT25i:4.0.4/4.1.B.0.587/tL1_3w:user/release-keys" \
+    PRIVATE_BUILD_DESC="MT25i-user 4.0.4 4.1.B.0.587 tL1_3w test-keys"
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := cm_phoenix
-PRODUCT_DEVICE := phoenix
+
+# Release name
+PRODUCT_RELEASE_NAME := phoenix
