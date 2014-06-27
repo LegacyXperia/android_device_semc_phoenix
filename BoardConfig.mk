c11c1d97bbdee22a70385650b351e377f6861441
Michael Bestas <mikeioannina@gmail.com>
2014-06-27 15:34:35 +0300

    phoenix: Use new defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,7 +19,7 @@
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/semc/phoenix/bluedroid
 
 # Kernel
-TARGET_KERNEL_CONFIG := nAa_phoenix_defconfig
+TARGET_KERNEL_CONFIG := lx_phoenix_defconfig
 
 # Sensors
 SOMC_CFG_SENSORS_ACCEL_BMA150_INPUT := yes
